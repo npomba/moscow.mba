abe0c04d302e8a93e8ea0fdcf1af738c01ed02f1
DrunkestManEvar <fullextentofsuperiority@mail.ru>
2021-07-15 20:32:06 +0300

    Extracted styling into a separate sass module

--- components/sections/Qna.tsx
+++ components/sections/Qna.tsx
@@ -1,10 +1,7 @@
 import stls from '@/styles/components/sections/Qna.module.sass'
-import classNames from 'classnames'
 import Popup from 'reactjs-popup'
 import PopupForm from '@/components/popups/PopupForm'
 import Accordion from '@/components/general/Accordion'
-
-import Script from 'next/script'
 
 const faq = [
   {
@@ -48,50 +45,43 @@ const faq = [
 
 const Qna = ({ programId, programTitle }) => {
   return (
-    <>
-      <section className='faq-section section-pl'>
-        <div className='title-pl red'>Узнай все</div>
-        <div className='faq-flex'>
-          <div className='faq-description'>
-            <h2>
-              Вопросы <br />и ответы
-            </h2>
-            <div className='desc'>
-              Задавайте вопросы на интересующую тему и наши менеджеры ответят
-              Вам в ближайшее время
-            </div>
-            <Popup
-              trigger={
-                <a className={classNames('button', stls.emptyBtn)}>
-                  Задать вопрос
-                </a>
-              }
-              modal
-              nested>
-              {close => (
-                <PopupForm
-                  programId={programId}
-                  programTitle={programTitle}
-                  title={'Получите консультацию'}
-                  closePopUpForm={close}
-                />
-              )}
-            </Popup>
-          </div>
-          <div className='faq-content'>
-            {faq.map((item, idx) => (
-              <Accordion
-                key={idx}
-                title={item.question}
-                accordionContent={item.answer}
-                isList={item.isList}
+    <section className={stls.container}>
+      <p className={stls.title}>Узнай все</p>
+      <div className={stls.flexContent}>
+        <div className={stls.descContainer}>
+          <h2 className={stls.descTitle}>
+            Вопросы <br />и ответы
+          </h2>
+          <p className={stls.desc}>
+            Задавайте вопросы на интересующую тему и наши менеджеры ответят Вам
+            в ближайшее время
+          </p>
+          <Popup
+            trigger={<a className={stls.button}>Задать вопрос</a>}
+            modal
+            nested>
+            {close => (
+              <PopupForm
+                programId={programId}
+                programTitle={programTitle}
+                title={'Получите консультацию'}
+                closePopUpForm={close}
               />
-            ))}
-          </div>
+            )}
+          </Popup>
         </div>
-      </section>
-      <Script src='/assets/js/accordion.js' strategy='lazyOnload' />
-    </>
+        <div className={stls.content}>
+          {faq.map((item, idx) => (
+            <Accordion
+              key={idx}
+              title={item.question}
+              accordionContent={item.answer}
+              isList={item.isList}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
   )
 }
 
