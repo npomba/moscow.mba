fe545ef64e8008c341f13b57e6f9da0c328c1b00
DrunkestManEvar <fullextentofsuperiority@mail.ru>
2021-07-19 19:58:57 +0300

    Extracted CostOfStudy styles, partially refactored Price and Loan components

--- components/costs/Loan.tsx
+++ components/costs/Loan.tsx
@@ -1,10 +1,12 @@
+import stls from '@/styles/components/costs/Loan.module.sass'
 import useAt from '@/components/hooks/useAt'
 
 const Loan = ({
   discount = false,
   type = null,
   format = null,
-  notComparingPrices = false
+  notComparingPrices = false,
+  renderedByComponent = null
 }) => {
   const at = useAt()
 
@@ -44,14 +46,43 @@ const Loan = ({
 
   const regularOrDiscounted = discount ? 'loanDiscounted' : 'loanRegular'
 
+  const componentSpecificClasses = {
+    simple: {},
+    new: {},
+    old: {
+      CostOfStudy: stls.costOfStudyOldPrice
+    }
+  }
+
+  const generalClasses = {
+    simple: stls.simplePrice,
+    new: stls.newPrice,
+    old: stls.oldPrice
+  }
+
+  const getPriceClass = (typeOfPrice, renderedByComponent) => {
+    if (!renderedByComponent) return `${typeOfPrice}-price`
+
+    const componentSpecificClass =
+      componentSpecificClasses[typeOfPrice]?.[renderedByComponent]
+    const generalClass = generalClasses[typeOfPrice]
+
+    return componentSpecificClass ? componentSpecificClass : generalClass
+  }
+
   return (
     <>
-      <i className={discount ? 'new-price' : 'simple-price'}>
+      <i
+        className={
+          discount
+            ? getPriceClass('new', renderedByComponent)
+            : getPriceClass('simple', renderedByComponent)
+        }>
         {price[regularOrDiscounted]?.[type]?.[format]} Р. / мес
       </i>
       {discount && !at.blended && !notComparingPrices && (
         <>
-          <i className='old-price'>
+          <i className={getPriceClass('old', renderedByComponent)}>
             {price.loanRegular[type]?.[format]} Р. / мес
           </i>
         </>
