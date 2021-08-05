dffdd1c2580eef745db901b09fbb1b9152f002ac
DrunkestManEvar <fullextentofsuperiority@mail.ru>
2021-08-05 20:10:26 +0300

    Added support for professions in multiple components

--- components/costs/TrainingPeriod.tsx
+++ components/costs/TrainingPeriod.tsx
@@ -1,14 +1,31 @@
 import SetString from '@/components/hooks/SetString'
-import langMenu from '@/data/translation/menu'
+import langMenu from '@/data/translation/months'
+import { getStringDeclensionNumber } from '@/components/utils'
 
 const TrainingPeriod = ({ type = null }) => {
   return (
     <>
-      {type === 'mini' && <>9 {SetString(langMenu.categoryMonth)}</>}
-      {type === 'professional' && <>18 {SetString(langMenu.categoryMonth)}</>}
-      {type === 'industry' && <>18 {SetString(langMenu.categoryMonth)}</>}
-      {type === 'profession' && <>4 {SetString(langMenu.categoryMonth)}</>}
-      {type === 'executive' && <>26 {SetString(langMenu.categoryMonth)}</>}
+      {type === 'mini' && (
+        <>9 {SetString(langMenu.months, false, getStringDeclensionNumber(9))}</>
+      )}
+      {type === 'professional' && (
+        <>
+          18 {SetString(langMenu.months, false, getStringDeclensionNumber(18))}
+        </>
+      )}
+      {type === 'industry' && (
+        <>
+          18 {SetString(langMenu.months, false, getStringDeclensionNumber(18))}
+        </>
+      )}
+      {type === 'profession' && (
+        <>4 {SetString(langMenu.months, false, getStringDeclensionNumber(4))}</>
+      )}
+      {type === 'executive' && (
+        <>
+          26 {SetString(langMenu.months, false, getStringDeclensionNumber(26))}
+        </>
+      )}
     </>
   )
 }
