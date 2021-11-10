6c9d532d20bd2cf11b449721332ca6a40fb145b7
Novailoveyou <novailoveyoux@gmail.com>
2021-11-10 13:56:19 +0300

    typo fix

--- data/translation/index.ts
+++ data/translation/index.ts
@@ -165,8 +165,8 @@ const index = {
   },
 
   mbaWorldDics: {
-    ru: 'В MBAcademy обучаются студенты из более чем 15 стран мира, среди которых Россия, Великобритания, США, Германия, Бельгия, Франция, Китай, Греция, Швейцария, Италия, Южная Африка и многие другие!',
-    en: 'Students from more than 15 countries get their education in MBAcademy, such as Russia, Great Britain, USA, Germany, Belgium, France, China, Greece, Switzerland, Italy, South Africa, and many others!'
+    ru: 'В Moscow Business Academy обучаются студенты из более чем 15 стран мира, среди которых Россия, Великобритания, США, Германия, Бельгия, Франция, Китай, Греция, Швейцария, Италия, Южная Африка и многие другие!',
+    en: 'Students from more than 15 countries get their education in Moscow Business Academy, such as Russia, Great Britain, USA, Germany, Belgium, France, China, Greece, Switzerland, Italy, South Africa, and many others!'
   },
 
   mbaWorldMoreThan: {
