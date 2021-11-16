8d72f6b552570104ed3f415fd0c5f855c420e1a6
MrAlexGert <alex.gert1812@.gmail.com>
2021-11-16 17:05:56 +0300

    margin discunt

--- components/general/Filters.tsx
+++ components/general/Filters.tsx
@@ -83,7 +83,7 @@ const Filters = ({
                   [stls.circle]: true,
                   [stls.active]: at.blended
                 })}></span>{' '}
-              BLENDED (с очными модулями)
+              BLENDED <br/> (с очными модулями)
             </a>
           </Link>
 
@@ -94,7 +94,7 @@ const Filters = ({
                   [stls.circle]: true,
                   [stls.active]: at.online
                 })}></span>{' '}
-              ONLINE (дистанционно){' '}
+              ONLINE <br/> (дистанционно){' '}
               <span className={stls.discount50}>
                 <Discount />
               </span>
