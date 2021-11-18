1c6e40545479798820867143011fe9d494fdafd9
MrAlexGert <alex.gert1812@.gmail.com>
2021-11-18 11:16:12 +0300

    refactoring1

--- components/general/Filters.tsx
+++ components/general/Filters.tsx
@@ -83,7 +83,7 @@ const Filters = ({
                   [stls.circle]: true,
                   [stls.active]: at.blended
                 })}></span>{' '}
-              BLENDED <br/> (с очными модулями)
+              BLENDED (с очными модулями)
             </a>
           </Link>
 
@@ -94,7 +94,7 @@ const Filters = ({
                   [stls.circle]: true,
                   [stls.active]: at.online
                 })}></span>{' '}
-              ONLINE <br/> (дистанционно){' '}
+              ONLINE (дистанционно){' '}
               <span className={stls.discount50}>
                 <Discount />
               </span>
