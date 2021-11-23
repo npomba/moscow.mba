f67d726646decfcd60c7a65af585b69fec5b0667
MrAlexGert <alex.gert1812@.gmail.com>
2021-11-23 17:40:47 +0300

    refactor5

--- helpers/MovingWithScroll.tsx
+++ helpers/MovingWithScroll.tsx
@@ -9,56 +9,44 @@ type MovingWithScrollType = {
 }
 
 const MovingWithScroll:React.FC<MovingWithScrollType> = ({children, start , end}) => {
-  const [parentH, setParentH] = useState(0)
-  const [h, setH] = useState(0)
   const ref: any = useRef()
+  const [move, setMove] = useState(0)
   const [scrollY, setScrollY] = useState(0)
   const [posTop, setPosTop] = useState<any>(0)
 
   const [scroll, setScroll] = useState('')
 
   useEffect(() => {
-    let elem = document.getElementById('myDiv');
-    let elemPosTop = elem.getBoundingClientRect();
-    setPosTop(elemPosTop.top)
-    console.log(elemPosTop.top)
-    setParentH(ref.current.offsetParent.offsetHeight)
-    setH(ref.current.offsetHeight)
-
     window.addEventListener('scroll', () => {
       setScrollY(window.scrollY)
-
     })
     positionElem()
-    console.log(scrollY)
   }, [scrollY])
 
+
   useEffect(() => {
-    // let elem = document.getElementById('myDiv');
-    // let elemPosTop = elem.getBoundingClientRect();
-    // setPosTop(elemPosTop.top)
-    // console.log(elemPosTop.top)
-    // setParentH(ref.current.offsetParent.offsetHeight)
-    // setH(ref.current.offsetHeight)
-
-    // console.log(elem.scrollHeight)
-
+    let elem = document.getElementById('__movingWithScroll');
+    let elemPosTop = elem.getBoundingClientRect();
+    setPosTop(elemPosTop.top)
+    setMove(ref.current.offsetParent.offsetHeight - ref.current.offsetHeight)
   }, [])
+
+
 
   const positionElem = () => {
     if (scrollY - posTop < 0) {
       return setScroll('0px')
-    } else if (scrollY - posTop > parentH - h) {
-      return setScroll(parentH - h + 'px')
+    } else if (scrollY - posTop > move) {
+      return setScroll(move + 'px')
     }
     return setScroll(scrollY - posTop + 'px')
   }
 
   return (
   <div
-    id={"myDiv"}
+    id={"__movingWithScroll"}
     ref={ref}
-    style={{position: 'absolute', top: scroll}}
+    style={{position: 'absolute', top: scroll }}
   >
    {children}
 </div>
