db30d43c73392eaac9f3fcd31e6aec3b4f71d1cb
novailoveyou <novailoveyoux@gmail.com>
2022-02-10 19:33:08 +0300

    set github enabled to false & organized types

--- types/index.ts
+++ types/index.ts
@@ -1,12 +1,12 @@
-import TypeCategories from '@/types/TypeCategories'
-import TypeClassNames from '@/types/TypeClassNames'
-import TypeImg from '@/types/TypeImg'
-import TypeOfType from '@/types/TypeOfType'
-import TypeProgram from '@/types/TypeProgram'
-import TypeProgramDefault from '@/types/TypeProgramDefault'
-import TypePrograms from '@/types/TypePrograms'
-import TypeProgramsDefault from '@/types/TypeProgramsDefault'
-import TypeStudyFormat from '@/types/TypeStudyFormat'
+import TypeCategories from '@/types/general/TypeCategories'
+import TypeClassNames from '@/types/general/TypeClassNames'
+import TypeImg from '@/types/general/TypeImg'
+import TypeOfType from '@/types/general/TypeOfType'
+import TypeProgram from '@/types/general/TypeProgram'
+import TypeProgramDefault from '@/types/general/TypeProgramDefault'
+import TypePrograms from '@/types/general/TypePrograms'
+import TypeProgramsDefault from '@/types/general/TypeProgramsDefault'
+import TypeStudyFormat from '@/types/general/TypeStudyFormat'
 
 export type {
   TypeCategories,
