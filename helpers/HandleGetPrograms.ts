23502cbb4aa5de9c5a1e530036a324fe76a71e3d
Novailoveyou <novailoveyoux@gmail.com>
2022-02-15 10:52:04 +0300

    linting in HandleGetPrograms

--- helpers/HandleGetPrograms.ts
+++ helpers/HandleGetPrograms.ts
@@ -1,14 +1,12 @@
 import { useContext, useEffect } from 'react'
 import programsContext from '@/context/programs/programsContext'
 
-
-const HandleGetPrograms = (data) => {
+const HandleGetPrograms = data => {
   const { setPrograms } = useContext(programsContext)
 
   useEffect(() => {
     setPrograms(data)
   }, [])
-
 }
 
 export default HandleGetPrograms
