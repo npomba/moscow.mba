0e73f27440ecafe1c476da28d5f492d6dea503cd
MrAlexGert <alex.gert1812@.gmail.com>
2022-02-17 17:48:02 +0300

    ImgMBA

---
+++ components/images/sections/ImgMBA.tsx
@@ -0,0 +1,23 @@
+import { TypeClassNames, TypeImg } from '@/types/index'
+import { ImgTemplate } from '@/components/images'
+import { useAt } from '@/helpers/index'
+import src from '@/public/assets/mba_text.png'
+
+const ImgMBA = ({
+  classNames,
+  width,
+  height
+}: TypeClassNames & TypeImg) => {
+  const at = useAt()
+  return (
+    <ImgTemplate
+      src={src}
+      width={width}
+      height={height}
+      alt={at.en ? 'Moscow Business Academy' : 'Moscow Business Academy'}
+      classNames={classNames}
+    />
+  )
+}
+
+export default ImgMBA
