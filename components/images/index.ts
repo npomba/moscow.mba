6b9a32c14dcd11fc8a9c028c17b6bf850be2d733
MrAlexGert <alex.gert1812@.gmail.com>
2022-02-17 17:32:51 +0300

    ImgIntramural

--- components/images/index.ts
+++ components/images/index.ts
@@ -7,6 +7,7 @@ import ImgDiscountDecoration from '@/components/images/newyears/ImgDiscountDecor
 import ImgStickyBottomDecoration from '@/components/images/newyears/ImgStickyBottomDecoration'
 import ImgStudentsDuringConference from '@/components/images/sections/ImgStudentsDuringConference'
 import ImgTemplate from '@/components/images/ImgTemplate'
+import ImgIntramural from '@/components/images/sections/ImgIntramural'
 
 export {
   ImgLogoEcicel,
@@ -17,5 +18,6 @@ export {
   ImgDiscountDecoration,
   ImgStickyBottomDecoration,
   ImgStudentsDuringConference,
-  ImgTemplate
+  ImgTemplate,
+  ImgIntramural
 }
