c649f1e0c9df08014c50a637ec2d168a31371f6e
MrAlexGert <alex.gert1812@.gmail.com>
2022-02-17 14:02:02 +0300

    ImgRosneft

---
+++ components/images/sections/ImgRosneft.tsx
@@ -0,0 +1,25 @@
+import { TypeClassNames, TypeImg } from '@/types/index'
+import { ImgTemplate } from '@/components/images'
+import { useAt } from '@/helpers/index'
+import src from '@/public/assets/images/clients/client_3.jpg'
+
+const ImgRosneft = ({
+    classNames,
+    width,
+    height
+}: TypeClassNames & TypeImg) => {
+    const at = useAt()
+    return (
+      <ImgTemplate
+        src={src}
+        width={width}
+        height={height}
+        alt={at.en ? 'Rosneft' : 'Роснефть'}
+        classNames={classNames}
+      />
+    )
+}  
+
+export default ImgRosneft
+
+
