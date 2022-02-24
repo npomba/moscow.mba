1129af4a69a917cbd332a07d0aeed17362d1683a
MrAlexGert <alex.gert1812@.gmail.com>
2022-02-24 15:56:23 +0300

    import_PageBlendedProgram

--- components/pages/BlendedProgram.tsx
+++ components/pages/BlendedProgram.tsx
@@ -2,27 +2,29 @@ import stls from '@/styles/components/pages/BlendedProgram.module.sass'
 import { NextSeo, CourseJsonLd } from 'next-seo'
 import { useRouter } from 'next/router'
 import truncate from 'truncate'
-import Reviews from '@/components/sections/Reviews'
-import JumbotronProgram from '@/components/sections/JumbotronProgram'
-import ProgramGoal from '@/components/sections/ProgramGoal'
-import WhatWillYouLearn from '@/components/sections/WhatWillYouLearn'
-import ProgramDesc from '@/components/sections/ProgramDesc'
-import HowProcessGoes from '@/components/sections/HowProcessGoes'
-import ProgramsModules from '@/components/sections/ProgramsModules'
-import ContactUs from '@/components/sections/ContactUs'
-import Qna from '@/components/sections/Qna'
-import Students from '@/components/sections/Students'
-import Teachers from '@/components/sections/Teachers'
-import UpToDateContent from '@/components/sections/UpToDateContent'
-import Diploma from '@/components/sections/Diploma'
-import CorporateClients from '@/components/sections/CorporateClients'
-import Accreditation from '@/components/sections/Accreditation'
-import Pros from '@/components/sections/Pros'
-import BlendedMetups from '@/components/sections/BlendedMetups'
-import CostOfStudy from '@/components/sections/CostOfStudy'
-import ECTSStandard from '@/components/sections/ECTSStandard'
-import CostOfStudyDescription from '@/components/sections/CostOfStudyDescription'
-import GetStudyPlan from '@/components/sections/GetStudyPlan'
+import {
+  Reviews,
+  JumbotronProgram,
+  ProgramGoal,
+  WhatWillYouLearn,
+  ProgramDesc,
+  HowProcessGoes,
+  ProgramsModules,
+  ContactUs,
+  Qna,
+  Students,
+  Teachers,
+  UpToDateContent,
+  Diploma,
+  CorporateClients,
+  Accreditation,
+  Pros,
+  BlendedMetups,
+  CostOfStudy,
+  ECTSStandard,
+  CostOfStudyDescription,
+  GetStudyPlan
+} from '@/components/sections'
 
 const PageBlendedProgram = ({ program, teachers }) => {
   const router = useRouter()
