05c65e320cc0a78cbbc3e5566a82efc1a2af6383
MrAlexGert <alex.gert1812@.gmail.com>
2022-02-24 16:29:15 +0300

    import_PageOnlineProgram

--- components/sections/index.tsx
+++ components/sections/index.tsx
@@ -18,6 +18,57 @@ import SectionJournalTags from '@/components/sections/journal/SectionJournalTags
 import SectionJournalTagsWithPictures from '@/components/sections/journal/SectionJournalTagsWithPictures'
 import SectionJournalTitle from '@/components/sections/journal/SectionJournalTitle'
 
+import About from '@/components/sections/About'
+import AboutExecutive from '@/components/sections/AboutExecutive'
+import Accreditation from '@/components/sections/Accreditation'
+import BlendedMetups from '@/components/sections/BlendedMetups'
+import CheckLicenses from '@/components/sections/CheckLicenses'
+import CompanyCard from '@/components/sections/CompanyCard'
+import ConferencesInEurope from '@/components/sections/ConferencesInEurope'
+import ContactUs from '@/components/sections/ContactUs'
+import CorporateClients from '@/components/sections/CorporateClients'
+import CostOfStudy from '@/components/sections/CostOfStudy'
+import CostOfStudyDescription from '@/components/sections/CostOfStudyDescription'
+import CourseOptions from '@/components/sections/CourseOptions'
+import CurrentLicenses from '@/components/sections/CurrentLicenses'
+import Diploma from '@/components/sections/Diploma'
+import ECTSStandard from '@/components/sections/ECTSStandard'
+import Executive from '@/components/sections/Executive'
+import ExecutiveRequirements from '@/components/sections/ExecutiveRequirements'
+import ForeignAffiliates from '@/components/sections/ForeignAffiliates'
+import GetStudyPlan from '@/components/sections/GetStudyPlan'
+import HelpWithEmployment from '@/components/sections/HelpWithEmployment'
+import HowProcessGoes from '@/components/sections/HowProcessGoes'
+import InPersonWithExperts from '@/components/sections/InPersonWithExperts'
+import JumbotronCta from '@/components/sections/JumbotronCta'
+import JumbotronMain from '@/components/sections/JumbotronMain'
+import JumbotronProgram from '@/components/sections/JumbotronProgram'
+import LegalDocuments from '@/components/sections/LegalDocuments'
+import MemberOfAcicel from '@/components/sections/MemberOfAcicel'
+import MemberOfRabe from '@/components/sections/MemberOfRabe'
+import ModulesAbroad from '@/components/sections/ModulesAbroad'
+import OurDiplomasAndCertificates from '@/components/sections/OurDiplomasAndCertificates'
+import ProgramDesc from '@/components/sections/ProgramDesc'
+import ProgramDevelopedStandard from '@/components/sections/ProgramDevelopedStandard'
+import ProgramGoal from '@/components/sections/ProgramGoal'
+import Programs from '@/components/sections/Programs'
+import ProgramsModules from '@/components/sections/ProgramsModules'
+import Pros from '@/components/sections/Pros'
+import Qna from '@/components/sections/Qna'
+import ResultsExecutive from '@/components/sections/ResultsExecutive'
+import Reviews from '@/components/sections/Reviews'
+import Rules from '@/components/sections/Rules'
+import Students from '@/components/sections/Students'
+import StudentsInternational from '@/components/sections/StudentsInternational'
+import Teachers from '@/components/sections/Teachers'
+import UpToDateContent from '@/components/sections/UpToDateContent'
+import WebinarCard from '@/components/sections/WebinarCard'
+import WebinarCards from '@/components/sections/WebinarCards'
+import WhatWillYouLearn from '@/components/sections/WhatWillYouLearn'
+import WhoItIsFor from '@/components/sections/WhoItIsFor'
+import WhoStudies from '@/components/sections/WhoStudies'
+
+
 export {
   SectionJournalAllArticles,
   SectionJournalBottom,
@@ -37,5 +88,56 @@ export {
   SectionJournalTagedArticles,
   SectionJournalTags,
   SectionJournalTagsWithPictures,
-  SectionJournalTitle
+  SectionJournalTitle,
+  
+  About,
+  WhoItIsFor,
+  WhoStudies,
+  WebinarCards,
+  WhatWillYouLearn,
+  WebinarCard,
+  UpToDateContent,
+  Teachers,
+  StudentsInternational,
+  Students,
+  Pros,
+  Qna,
+  ResultsExecutive,
+  Reviews,
+  Rules,
+  ProgramsModules,
+  Programs,
+  OurDiplomasAndCertificates,
+  ProgramDesc,
+  ProgramDevelopedStandard,
+  ProgramGoal,
+  JumbotronProgram,
+  LegalDocuments,
+  MemberOfAcicel,
+  MemberOfRabe,
+  ModulesAbroad,
+  InPersonWithExperts,
+  JumbotronCta,
+  JumbotronMain,
+  AboutExecutive,
+  Accreditation,
+  BlendedMetups,
+  CheckLicenses,
+  CompanyCard,
+  ConferencesInEurope,
+  ContactUs,
+  CorporateClients,
+  CostOfStudy,
+  CostOfStudyDescription,
+  CourseOptions,
+  CurrentLicenses,
+  Diploma,
+  ECTSStandard,
+  Executive,
+  ExecutiveRequirements,
+  ForeignAffiliates,
+  GetStudyPlan,
+  HelpWithEmployment,
+  HowProcessGoes,
+
 }
