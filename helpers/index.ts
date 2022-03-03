233472e5f1ec5cc0eb2ff2511cb0567606d85df2
Novailoveyou <novailoveyoux@gmail.com>
2022-03-03 19:49:24 +0300

    made easier to work with export index file in ./helpers/index

--- helpers/index.ts
+++ helpers/index.ts
@@ -1,59 +1,28 @@
-import createBlended from '@/helpers/general/createBlended'
-import createLeadBackApi from '@/helpers/general/createLeadBackApi'
-import createProgramModules from '@/helpers/general/createProgramModules'
-import createProgramModulesBase from '@/helpers/general/createProgramModulesBase'
-import createProgramModulesSpecialized from '@/helpers/general/createProgramModulesSpecialized'
-import fetchProgram from '@/helpers/general/fetchProgram'
-import fetchPrograms from '@/helpers/general/fetchPrograms'
-import fetchProgramsGetStaticProps from '@/helpers/general/fetchProgramsGetStaticProps'
-import fetchProgramsGetStaticPropsCourse from '@/helpers/general/fetchProgramsGetStaticPropsCourse'
-import fetchProgramsGetStaticPropsProfession from '@/helpers/general/fetchProgramsGetStaticPropsProfession'
-import fetchProgramsGetStaticPropsPromo from '@/helpers/general/fetchProgramsGetStaticPropsPromo'
-import getClassNames from '@/helpers/general/getClassNames'
-import getImageHeight from '@/helpers/general/getImageHeight'
-import getProgram from '@/helpers/general/getProgram'
-import getProgramsReducedData from '@/helpers/general/getProgramsReducedData'
-import fetchPaths from '@/helpers/general/fetchPaths'
-import getStringDeclensionNumber from '@/helpers/general/getStringDeclensionNumber'
-import HandleGetPrograms from '@/helpers/general/HandleGetPrograms'
-import handleGetStaticPaths from '@/helpers/general/handleGetStaticPaths'
-import handleGetStaticProps from '@/helpers/general/handleGetStaticProps'
-import handlePlaceholder from '@/helpers/general/handlePlaceholder'
-import hitContactRoute from '@/helpers/general/hitContactRoute'
-import getBackJWTToken from '@/helpers/general/getBackJWTToken'
-import onSubmitForm from '@/helpers/general/onSubmitForm'
-import SetString from '@/helpers/general/SetString'
-import toNumberWithSpaces from '@/helpers/general/toNumberWithSpaces'
-import useAt from '@/helpers/general/useAt'
-import useWindowWidth from '@/helpers/general/useWindowWidth'
-
-export {
-  createBlended,
-  createLeadBackApi,
-  createProgramModules,
-  createProgramModulesBase,
-  createProgramModulesSpecialized,
-  fetchProgram,
-  fetchPrograms,
-  fetchProgramsGetStaticProps,
-  fetchProgramsGetStaticPropsCourse,
-  fetchProgramsGetStaticPropsProfession,
-  fetchProgramsGetStaticPropsPromo,
-  getClassNames,
-  getImageHeight,
-  getProgram,
-  getProgramsReducedData,
-  fetchPaths,
-  getStringDeclensionNumber,
-  HandleGetPrograms,
-  handleGetStaticPaths,
-  handleGetStaticProps,
-  handlePlaceholder,
-  hitContactRoute,
-  getBackJWTToken,
-  onSubmitForm,
-  SetString,
-  toNumberWithSpaces,
-  useAt,
-  useWindowWidth
-}
+export { default as createBlended } from '@/helpers/general/createBlended'
+export { default as createLeadBackApi } from '@/helpers/general/createLeadBackApi'
+export { default as createProgramModules } from '@/helpers/general/createProgramModules'
+export { default as createProgramModulesBase } from '@/helpers/general/createProgramModulesBase'
+export { default as createProgramModulesSpecialized } from '@/helpers/general/createProgramModulesSpecialized'
+export { default as fetchProgram } from '@/helpers/general/fetchProgram'
+export { default as fetchPrograms } from '@/helpers/general/fetchPrograms'
+export { default as fetchProgramsGetStaticProps } from '@/helpers/general/fetchProgramsGetStaticProps'
+export { default as fetchProgramsGetStaticPropsCourse } from '@/helpers/general/fetchProgramsGetStaticPropsCourse'
+export { default as fetchProgramsGetStaticPropsProfession } from '@/helpers/general/fetchProgramsGetStaticPropsProfession'
+export { default as fetchProgramsGetStaticPropsPromo } from '@/helpers/general/fetchProgramsGetStaticPropsPromo'
+export { default as getClassNames } from '@/helpers/general/getClassNames'
+export { default as getImageHeight } from '@/helpers/general/getImageHeight'
+export { default as getProgram } from '@/helpers/general/getProgram'
+export { default as getProgramsReducedData } from '@/helpers/general/getProgramsReducedData'
+export { default as fetchPaths } from '@/helpers/general/fetchPaths'
+export { default as getStringDeclensionNumber } from '@/helpers/general/getStringDeclensionNumber'
+export { default as HandleGetPrograms } from '@/helpers/general/HandleGetPrograms'
+export { default as handleGetStaticPaths } from '@/helpers/general/handleGetStaticPaths'
+export { default as handleGetStaticProps } from '@/helpers/general/handleGetStaticProps'
+export { default as handlePlaceholder } from '@/helpers/general/handlePlaceholder'
+export { default as hitContactRoute } from '@/helpers/general/hitContactRoute'
+export { default as getBackJWTToken } from '@/helpers/general/getBackJWTToken'
+export { default as onSubmitForm } from '@/helpers/general/onSubmitForm'
+export { default as SetString } from '@/helpers/general/SetString'
+export { default as toNumberWithSpaces } from '@/helpers/general/toNumberWithSpaces'
+export { default as useAt } from '@/helpers/general/useAt'
+export { default as useWindowWidth } from '@/helpers/general/useWindowWidth'
