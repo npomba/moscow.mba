e946b7148648b7029146424d34ff3081bee2670b
Novailoveyou <novailoveyoux@gmail.com>
2022-03-03 19:47:45 +0300

    made easier to work with export index file in ./context/index

--- context/index.ts
+++ context/index.ts
@@ -1,43 +1,20 @@
 // ./journal
-import ContextJournalContext from '@/context/journal/contextJournalContext'
-import contextJournalReducer from '@/context/journal/contextJournalReducer'
-import ContextJournalState from '@/context/journal/ContextJournalState'
+export { default as ContextJournalContext } from '@/context/journal/contextJournalContext'
+export { default as contextJournalReducer } from '@/context/journal/contextJournalReducer'
+export { default as ContextJournalState } from '@/context/journal/ContextJournalState'
 // ./menu
-import MenuContext from '@/context/menu/menuContext'
-import menuReducer from '@/context/menu/menuReducer'
-import MenuState from '@/context/menu/MenuState'
+export { default as MenuContext } from '@/context/menu/menuContext'
+export { default as menuReducer } from '@/context/menu/menuReducer'
+export { default as MenuState } from '@/context/menu/MenuState'
 // ./overlay
-import OverlayContext from '@/context/overlay/overlayContext'
-import overlayReducer from '@/context/overlay/overlayReducer'
-import OverlayState from '@/context/overlay/OverlayState'
+export { default as OverlayContext } from '@/context/overlay/overlayContext'
+export { default as overlayReducer } from '@/context/overlay/overlayReducer'
+export { default as OverlayState } from '@/context/overlay/OverlayState'
 // ./program
-import ProgramContext from '@/context/program/programContext'
-import programReducer from '@/context/program/programReducer'
-import ProgramState from '@/context/program/ProgramState'
+export { default as ProgramContext } from '@/context/program/programContext'
+export { default as programReducer } from '@/context/program/programReducer'
+export { default as ProgramState } from '@/context/program/ProgramState'
 // ./programs
-import ProgramsContext from '@/context/programs/programsContext'
-import programsReducer from '@/context/programs/programsReducer'
-import ProgramsState from '@/context/programs/ProgramsState'
-
-export {
-  // ./journal
-  ContextJournalContext,
-  contextJournalReducer,
-  ContextJournalState,
-  // ./menu
-  MenuContext,
-  menuReducer,
-  MenuState,
-  // ./overlay
-  OverlayContext,
-  overlayReducer,
-  OverlayState,
-  // ./program
-  ProgramContext,
-  programReducer,
-  ProgramState,
-  // ./programs
-  ProgramsContext,
-  programsReducer,
-  ProgramsState
-}
+export { default as ProgramsContext } from '@/context/programs/programsContext'
+export { default as programsReducer } from '@/context/programs/programsReducer'
+export { default as ProgramsState } from '@/context/programs/ProgramsState'
