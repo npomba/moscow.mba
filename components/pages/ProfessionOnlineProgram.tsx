297c85169187cbea759ba4f833396e1ff6f49f2a
Novailoveyou <novailoveyoux@gmail.com>
2022-03-11 11:37:50 +0300

    added falsy prove to profession pages

--- components/pages/ProfessionOnlineProgram.tsx
+++ components/pages/ProfessionOnlineProgram.tsx
@@ -29,12 +29,12 @@ const ProfessionOnlineProgram = ({ program, teachers }) => {
   return (
     <>
       <NextSeo
-        title={`${data.title} MBA - Moscow Business Academy`}
+        title={`${data?.title} MBA - Moscow Business Academy`}
         description={truncate(program.goal, 120)}
         canonical={`https://moscow.mba${router.asPath}`}
       />
       <CourseJsonLd
-        courseName={`${data.title} MBA`}
+        courseName={`${data?.title} MBA`}
         provider={{
           name: 'Moscow Business Academy',
           url: `https://moscow.mba${router.asPath}`
@@ -51,24 +51,24 @@ const ProfessionOnlineProgram = ({ program, teachers }) => {
       <ProgramsModules program={data} smallerMb />
       <GetStudyPlan />
       <Teachers
-        programId={data._id}
-        programTitle={data.title}
+        programId={data?._id}
+        programTitle={data?.title}
         teachers={program.teachers}
       />
       <HelpWithEmployment />
       <CorporateClients partnershipTitle />
       <Diploma />
       <CostOfStudy
-        programId={data._id}
-        programTitle={data.title}
-        programFormat={data.studyFormat}
-        programType={data.category?.type}
-        programPrice={data.price}
+        programId={data?._id}
+        programTitle={data?.title}
+        programFormat={data?.studyFormat}
+        programType={data?.category?.type}
+        programPrice={data?.price}
       />
-      <Qna programId={data._id} programTitle={data.title} />
+      <Qna programId={data?._id} programTitle={data?.title} />
       <ContactUs
-        programId={data._id}
-        programTitle={data.title}
+        programId={data?._id}
+        programTitle={data?.title}
         title={'Не знаете что выбрать?'}
         titleNewStr={'Получите консультацию по программам'}
         overlapsFooter
