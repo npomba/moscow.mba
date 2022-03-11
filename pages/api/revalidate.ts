f71d69718be180a666d6a76d37c6bf4f9a7e8237
Novailoveyou <novailoveyoux@gmail.com>
2022-03-11 14:48:37 +0300

    added revalidate api route

---
+++ pages/api/revalidate.ts
@@ -0,0 +1,15 @@
+const revalidate = async (req, res) => {
+  if (req.query.secret !== process.env.REVALIDATE_TOKEN) {
+    return res.status(401).json({ message: 'Invalid token' })
+  }
+
+  try {
+    await res.unstable_revalidate(req.query.path)
+    return res.json({ revalidated: true })
+  } catch (err) {
+    console.log(err)
+    return res.status(500).send('Error revalidating')
+  }
+}
+
+export default revalidate
