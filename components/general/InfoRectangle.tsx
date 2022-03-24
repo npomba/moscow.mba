0ca9392f38c553aca5b155c081eec49a58cf63bf
Novailoveyou <novailoveyoux@gmail.com>
2022-03-24 22:59:54 +0300

    fixed import build errors

--- components/general/InfoRectangle.tsx
+++ components/general/InfoRectangle.tsx
@@ -4,8 +4,6 @@ import { useAt } from '@/hooks/index'
 // import { Wrapper } from '@/components/layout'
 import { PopupInfo } from '@/components/popups'
 import { Until, TrainingPeriod } from '@/components/costs'
-import lang from '@/data/translation/index'
-import langMenu from '@/data/translation/menu'
 
 // TODO:: improve styles for rectangle. It's content is not vertically centered on tablet & laptop, it goes full width on smaller screen but should have padding left & right
 const InfoRectangle = ({
