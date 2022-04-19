767624b91f9ab1501de39653184c88409de6c304
novailoveyou <novailoveyoux@gmail.com>
2022-04-19 11:39:51 +0300

    brought back IconClose and IconSearch

--- components/icons/IconClose.tsx
+++ components/icons/IconClose.tsx
@@ -11,24 +11,24 @@ type TIconClose = TypeClassNames & {
 
 const IconClose = ({ classNames, stroke = '#000' }: TIconClose) => {
   return (
-    <>IconClose</>
-    // <GeneralIconContainer classNames={[cn(stls.container, classNames)]}>
-    // <svg viewBox='0 0 14 14' fill='none' xmlns='http://www.w3.org/2000/svg'>
-    //   <title>Закрыть</title>
-    //   <path
-    //     d='M1 1L13 13'
-    //     stroke={stroke || colors.omega}
-    //     strokeLinecap='round'
-    //     strokeLinejoin='round'
-    //   />
-    //   <path
-    //     d='M13 1L1 13'
-    //     stroke={stroke || colors.omega}
-    //     strokeLinecap='round'
-    //     strokeLinejoin='round'
-    //   />
-    // </svg>
-    // </GeneralIconContainer>
+    // <>IconClose</>
+    <GeneralIconContainer classNames={[cn(stls.container, classNames)]}>
+      <svg viewBox='0 0 14 14' fill='none' xmlns='http://www.w3.org/2000/svg'>
+        <title>Закрыть</title>
+        <path
+          d='M1 1L13 13'
+          stroke={stroke || colors.omega}
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        />
+        <path
+          d='M13 1L1 13'
+          stroke={stroke || colors.omega}
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        />
+      </svg>
+    </GeneralIconContainer>
   )
 }
 
